c1e83da0650a4dc028264abeee5fc36138669335
ADSM <yesform2018@gmail.com>
2022-02-17 05:23:05 +0600

    Calculat part is finished

--- js/script.js
+++ js/script.js
@@ -9,20 +9,39 @@ function getTotalExpenses() {
     const foodCost = getCost('food');
     const rentCost = getCost('rent');
     const clothesCost = getCost('clothes');
+    if (isNaN(foodCost)) {
+        document.getElementById('food-erorr')
+    }
     const totalExpenses = foodCost + rentCost + clothesCost;
     return totalExpenses;
 }
-// Expenses calculating
-document.getElementById('calculat-btn').addEventListener('click', function () {
+function getIncomeAmount() {
     const incomeInput = document.getElementById('income-input').value;
     const income = parseFloat(incomeInput);
-
-    const totalCostText = document.getElementById('total-expenses');
-    const totalCost = parseFloat(totalCostText);
+    return income;
+}
+// Get cost and balence updated
+function getUpdated() {
+    // Update total expenses
+    const totalExpensesText = document.getElementById('total-expenses');
     const totalExpenses = getTotalExpenses();
-    totalCostText.innerText = totalExpenses;
-
-    const remainingBalanceText = document.getElementById('remaining-balance');
-    const remainingBalance = parseFloat(remainingBalanceText.innerText)
-    remainingBalanceText.innerText = income - totalExpenses;
+    totalExpensesText.innerText = totalExpenses;
+    // Update remaining balance
+    const balanceText = document.getElementById('balance');
+    balanceText.innerText = getIncomeAmount() - totalExpenses;
+}
+// Update savings
+document.getElementById('save-btn').addEventListener('click', function () {
+    const savePersentes = parseFloat(document.getElementById('save-input').value);
+    const remainingBalance = document.getElementById('remaining-balance');
+    const balanceText = document.getElementById('balance');
+    const balance = parseFloat(balanceText.innerText)
+    const saveAmount = document.getElementById('save-amount');
+    const saveings = (savePersentes * getIncomeAmount()) / 100;
+    saveAmount.innerText = saveings;
+    remainingBalance.innerText = balance - saveings;
+})
+// Expenses calculating
+document.getElementById('calculat-btn').addEventListener('click', function () {
+    getUpdated()
 })
