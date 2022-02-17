b1ca9fa83a86162cc78b3892c46a674395a28f13
ADSM <yesform2018@gmail.com>
2022-02-17 06:43:57 +0600

    project is completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+// Get income amount
+function getIncomeAmount() {
+    const incomeInput = document.getElementById('income-input').value;
+    const income = parseFloat(incomeInput);
+    if (incomeInput < 0) {
+        document.getElementById('incom-erorr').style.display = 'block';
+        incomeInput = '';
+    }
+    else {
+        document.getElementById('incom-erorr').style.display = 'none';
+        return income;
+    }
+}
 // Get expenses total
 function getCost(costFor) {
     const costInput = document.getElementById(costFor + '-input').value;
@@ -32,11 +45,7 @@ function getTotalExpenses() {
         return totalExpenses;
     }
 }
-function getIncomeAmount() {
-    const incomeInput = document.getElementById('income-input').value;
-    const income = parseFloat(incomeInput);
-    return income;
-}
+
 // Get cost and balence updated
 function getUpdated() {
     // Update total expenses
@@ -45,20 +54,33 @@ function getUpdated() {
     totalExpensesText.innerText = totalExpenses;
     // Update remaining balance
     const balanceText = document.getElementById('balance');
-    balanceText.innerText = getIncomeAmount() - totalExpenses;
+    if (totalExpenses < getIncomeAmount()) {
+        balanceText.innerText = getIncomeAmount() - totalExpenses;
+    }
+    else {
+        alert('!your income is less expenses')
+    }
 }
+
+// Expenses calculating
+document.getElementById('calculat-btn').addEventListener('click', function () {
+    getIncomeAmount()
+    getUpdated();
+})
 // Update savings
 document.getElementById('save-btn').addEventListener('click', function () {
     const savePersentes = parseFloat(document.getElementById('save-input').value);
     const remainingBalance = document.getElementById('remaining-balance');
-    const balanceText = document.getElementById('balance');
-    const balance = parseFloat(balanceText.innerText)
+    const balanceText = document.getElementById('balance').innerText;
+    const balance = parseFloat(balanceText)
     const saveAmount = document.getElementById('save-amount');
-    const saveings = (savePersentes * getIncomeAmount()) / 100;
-    saveAmount.innerText = saveings;
-    remainingBalance.innerText = balance - saveings;
-})
-// Expenses calculating
-document.getElementById('calculat-btn').addEventListener('click', function () {
-    getUpdated()
+    if (savePersentes < 0) {
+        alert('!Please inter a posetive number')
+        savePersentes = '';
+    }
+    else {
+        const saveings = (savePersentes * getIncomeAmount()) / 100;
+        saveAmount.innerText = saveings;
+        remainingBalance.innerText = balance - saveings;
+    }
 })
